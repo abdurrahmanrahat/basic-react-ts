e710c1d9e2c141b50e63affb318a40e60e030a45
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-11 14:56:03 +0600

    react-ts project initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+A live practice-project using react, react-dom, typescript, tailwind, and some more tech.
---
+++ src/pages/UseStateExample.tsx
@@ -0,0 +1,9 @@
+const UseStateExample = () => {
+  return (
+    <div>
+      <h2 className="text-purple-300 text-4xl mx-auto">Hello</h2>
+    </div>
+  );
+};
+
+export default UseStateExample;
