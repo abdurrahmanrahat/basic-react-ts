ea4f0cc6dbcf0e3351496cce91371b766b6d6695
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-11 15:51:19 +0600

    Async Behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import UseStateExample from "./pages/UseStateExample";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <UseStateExample></UseStateExample>
+      <UseStateExample
+        counter={counter}
+        setCounter={setCounter}
+      ></UseStateExample>
     </div>
   );
 }
--- src/pages/UseStateExample.tsx
+++ src/pages/UseStateExample.tsx
@@ -1,27 +1,31 @@
-import { useState } from "react";
+import React from "react";
 
-const UseStateExample = () => {
-  const [counter, setCounter] = useState(0);
+type TCount = {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+};
 
-  console.log("render");
-  // handleIncrement
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  };
+const UseStateExample = ({ counter, setCounter }: TCount) => {
+  //   const handleIncrement = () => {
+  //     setCounter((prevState) => prevState + 1);
+  //   };
+
+  //   const handleAsyncIncrement = () => {
+  //     setTimeout(() => {
+  //       setCounter((prevState) => prevState + 1);
+  //     }, 2000);
+  //   };
 
   return (
     <div className="text-center">
       <h2 className="text-purple-400 text-4xl mx-auto">Count</h2>
       <h4 className="text-4xl font-semibold">{counter}</h4>
-      <button onClick={handleIncrement} className="btn-primary">
+      <button onClick={() => setCounter(counter + 1)} className="btn-primary">
         Increment
       </button>
-      <button
-        onClick={() => setCounter((prevStatus) => prevStatus - 1)}
-        className="btn-danger"
-      >
-        Decrement
-      </button>
+      {/* <button onClick={handleAsyncIncrement} className="btn-danger">
+        Async Inc
+      </button> */}
       <button onClick={() => setCounter(0)} className="btn-secondary">
         Reset
       </button>
