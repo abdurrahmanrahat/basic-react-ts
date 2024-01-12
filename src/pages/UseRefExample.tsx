74222dc3a5ec6cf4984bf0518c535b0334b223f9
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-01-12 17:21:12 +0600

    useRef with input element

--- src/pages/UseRefExample.tsx
+++ src/pages/UseRefExample.tsx
@@ -1,20 +1,38 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 const UseRefExample = () => {
-  const [count, setCount] = useState(0);
-  const myRef = useRef(0);
+  //   const [count, setCount] = useState(0);
+  const myRef = useRef<HTMLInputElement | null>(null);
 
-  const increment = () => {
-    myRef.current = myRef.current + 1;
-    setCount(count + 1);
-  };
+  //   const increment = () => {
+  //     myRef.current = myRef.current + 1;
+  //     setCount(count + 1);
+  //   };
+
+  //   const handleSubmit = (e: React.FormEvent) => {
+  //     e.preventDefault();
+  //     console.log(myRef.current.value);
+  //   };
+
+  useEffect(() => {
+    myRef.current?.focus();
+  }, []);
 
   return (
     <div>
       <h2>useRef</h2>
-      <button onClick={() => increment()} className="btn-secondary">
-        {myRef.current}
-      </button>
+      <form>
+        <input
+          ref={myRef}
+          type="text"
+          name="name"
+          id="name"
+          className="border-2 border-purple-600"
+        />
+        <button type="submit" className="btn-secondary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
